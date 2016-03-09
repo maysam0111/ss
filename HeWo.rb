7486dad2d58477d8882d0b89fdaf0d4e137714f8
maysam0111 <madamp90@gmail.com>
2016-03-09 16:37:03 +0330

    Delete HeWo.rb

--- HeWo.rb
+++
@@ -1,9 +0,0 @@
-# Project: Hello World
-# Author: CCSF Coders
-# Description: Prints "Hello World" in
-# different spoken languages in ruby
-
-#to run: ruby HeWo.rb
-
-puts "Hello World! - English" #Coders
-puts "Ciao Mondo! - Italian" #Jason Hoang
